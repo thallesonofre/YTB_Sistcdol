cd4de561ae5159a2afc2c8f0645d4ee7fc71bb65
Thalles Onofre <thalles.onofre@gmail.com>
2024-09-10 14:57:29 -0300

    Atualização do vídeo 13

---
+++ modulos/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from .views import ClienteCreate
+from .views import ClienteUpdate
+from .views import ClienteDelete
+from .views import ClienteList
+
+
+
+urlpatterns = [
+    #create
+    path('cadastrar/cliente', ClienteCreate.as_view(), name="cadastrar_cliente"),
+    
+    
+    #Update
+    path('editar/cliente/<int:pk>', ClienteUpdate.as_view(), name="editar_cliente"),
+    
+    
+    #Delete
+    path('excluir/cliente/<int:pk>', ClienteDelete.as_view(), name="excluir_cliente"),
+    
+    
+    #Liste
+    path('listar/clientes', ClienteList.as_view(), name="listar_clientes"),
+    
+    
+]
---
+++ setup/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from paginas.views import Index
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('modulos.urls')),
+    
+    #pagina inicial
+    path('', Index.as_view(), name="index"),
+]
